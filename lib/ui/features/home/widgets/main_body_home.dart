15094d194b09b4fe68c64e6ef2da7c963bb7ca84
aliakberaakash <ali852609@gmail.com>
2020-11-11 02:40:35 +0600

    added food panda like scroll

--- lib/ui/features/home/widgets/main_body_home.dart
+++ lib/ui/features/home/widgets/main_body_home.dart
@@ -3,6 +3,7 @@ import 'package:food_panda_clone/data/models/restaurant_details/food_category.da
 import 'package:food_panda_clone/ui/features/home/widgets/category_widget.dart';
 import 'package:food_panda_clone/utils/data.dart';
 import 'package:food_panda_clone/utils/my_colors.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 class MainBodyHome extends StatefulWidget {
   @override
@@ -16,11 +17,18 @@ class _MainBodyHomeState extends State<MainBodyHome> {
 
   List<FoodCategory> tabTextList;
   List<Tab> tabs;
+  final ItemScrollController itemScrollController = ItemScrollController();
+  final ItemPositionsListener itemPositionsListener = ItemPositionsListener.create();
+  TabController tabController;
 
   @override
   void initState() {
     tabTextList = APP_DATA.keys.toList(growable: true);
     tabs = tabTextList.map((e) => Tab(text: e.name,)).toList(growable: true);
+    itemPositionsListener.itemPositions.addListener(() {
+      ItemPosition pos = itemPositionsListener.itemPositions.value.elementAt(0);
+      tabController.animateTo(pos.index, duration: Duration(milliseconds: 500));
+    });
 
     super.initState();
   }
@@ -32,11 +40,14 @@ class _MainBodyHomeState extends State<MainBodyHome> {
       child: Builder(
         builder: (context) {
 
-          final TabController tabController = DefaultTabController.of(context);
+          tabController = DefaultTabController.of(context);
           tabController.addListener(() {
             if (!tabController.indexIsChanging) {
-              // Your code goes here.
-              // To get index of current tab use tabController.index
+              itemScrollController.scrollTo(
+                index: tabController.index,
+                duration: Duration(milliseconds: 500),
+                curve: Curves.easeInOutCubic
+              );
             }
           });
 
@@ -48,19 +59,21 @@ class _MainBodyHomeState extends State<MainBodyHome> {
                 tabs: tabs,
               ),
             ),
-            body: ListView.builder(
+            body: ScrollablePositionedList.builder(
               itemCount: tabTextList.length,
-                itemBuilder: (context, index){
-                  return Column(
-                    children: [
-                      Container(
-                        color: light_grey1,
-                        height: (index==0) ? 0 : 10,
-                      ),
-                      CategoryWidget(category:tabTextList[index], itemList: APP_DATA[tabTextList[index]],),
-                    ],
-                  );
-                }
+              itemBuilder: (context, index){
+                return Column(
+                  children: [
+                    Container(
+                      color: light_grey1,
+                      height: (index==0) ? 0 : 10,
+                    ),
+                    CategoryWidget(category:tabTextList[index], itemList: APP_DATA[tabTextList[index]],),
+                  ],
+                );
+              },
+              itemScrollController: itemScrollController,
+              itemPositionsListener: itemPositionsListener,
             ),
           );
         }
